885296f3fa0b8308e2be329a80464ecf6b497981
Guillaume Abadie <guillaume.abadie@gmail.com>
2014-01-23 22:52:16 +0100

    Compiles library's C files

--- default.mk
+++ default.mk
@@ -1,7 +1,14 @@
 
-LIBHEADER_PRODUCT := $(call product_create,BINHEADERS,headers)
-LIBHEADER_TARGET := $(call product_target,$(LIBHEADER_PRODUCT))
-$(call product_public,$(LIBHEADER_PRODUCT))
+LIB_HEADERS_PRODUCT := $(call product_create,BINHEADERS,headers)
+LIB_HEADERS_TARGET := $(call product_target,$(LIB_HEADERS_PRODUCT))
+$(call product_public,$(LIB_HEADERS_PRODUCT))
 
-$(LIBHEADER_TARGET): $(call bin_header_deps,srclib/yotta.h)
+$(LIB_HEADERS_TARGET): $(call bin_header_deps,./srclib/yotta.h)
+
+
+LIB_BINARIES_PRODUCT := $(call product_create,BINLIBSTATIC,static_lib)
+LIB_BINARIES_TARGET := $(call product_target,$(LIB_BINARIES_PRODUCT))
+$(call product_public,$(LIB_BINARIES_PRODUCT))
+
+$(LIB_BINARIES_TARGET): $(call bin_object_files,$(call rwildcard,./srclib/,*.c))
 
