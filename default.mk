845bbf97788bc1568af7cb4c143c104b5d5b7037
Guillaume Abadie <guillaume.abadie@gmail.com>
2014-01-26 00:52:19 +0100

    mv srclib -> src

--- default.mk
+++ default.mk
@@ -5,7 +5,7 @@ LIB_HEADERS_PRODUCT := $(call product_create,BINHEADERS,headers)
 LIB_HEADERS_TARGET := $(call product_target,$(LIB_HEADERS_PRODUCT))
 $(call product_public,$(LIB_HEADERS_PRODUCT))
 
-$(LIB_HEADERS_TARGET): $(call bin_header_deps,./srclib/yotta.h)
+$(LIB_HEADERS_TARGET): $(call bin_header_deps,./src/yotta.h)
 
 
 # ------------------------------------------------------------------------------ Yotta library's binaries
@@ -14,7 +14,7 @@ LIB_BINARIES_PRODUCT := $(call product_create,BINLIBSTATIC,static_lib)
 LIB_BINARIES_TARGET := $(call product_target,$(LIB_BINARIES_PRODUCT))
 $(call product_public,$(LIB_BINARIES_PRODUCT))
 
-$(LIB_BINARIES_TARGET): $(call bin_object_files,$(call rwildcard,./srclib/,*.c))
+$(LIB_BINARIES_TARGET): $(call bin_object_files,$(call rwildcard,./src/,*.c))
 
 
 # ------------------------------------------------------------------------------ Yotta library's tests
