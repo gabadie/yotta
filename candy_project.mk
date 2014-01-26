00de1e90a84b2dccf8dce560820ff3330c6ec7c0
Guillaume Abadie <guillaume.abadie@gmail.com>
2014-01-26 17:27:00 +0100

    Lets candy_project.mk uses *DEPS variables

--- candy_project.mk
+++ candy_project.mk
@@ -18,7 +18,11 @@ LIB_BINARIES_PRODUCT := $(call product_create,BINLIBSTATIC,static_lib)
 LIB_BINARIES_TARGET := $(call product_target,$(LIB_BINARIES_PRODUCT))
 $(call product_public,$(LIB_BINARIES_PRODUCT))
 
-$(LIB_BINARIES_TARGET): $(call bin_object_files,$(call filelist,./src/yotta.flist))
+LIB_OBJECT_BINARIES := $(call bin_object_files,$(call filelist,./src/yotta.flist))
+
+# ------------------------------------------------------------ link configuration
+$(LIB_BINARIES_TARGET): $(LIB_OBJECT_BINARIES)
+$(LIB_BINARIES_TARGET): LDFLAGS += $(LIB_OBJECT_BINARIES)
 
 
 # ------------------------------------------------------------------------------ Yotta slave application
@@ -27,20 +31,31 @@ SLAVE_PRODUCT := $(call product_create,BINEXEC,yotta_slave)
 SLAVE_TARGET := $(call product_target,$(SLAVE_PRODUCT))
 $(call product_public,$(SLAVE_PRODUCT))
 
-$(SLAVE_TARGET): $(call bin_object_files,$(call filelist,./src/slave.flist))
+SLAVE_BINARY_OBJECTS := $(call bin_object_files,$(call filelist,./src/slave.flist))
+
+# ------------------------------------------------------------ link configuration
+$(SLAVE_TARGET): $(SLAVE_BINARY_OBJECTS)
+$(SLAVE_TARGET): LDFLAGS += $(SLAVE_BINARY_OBJECTS)
 
 
 # ------------------------------------------------------------------------------ Yotta library's tests
 
-$(foreach TEST,$(call filelist,tests/testlib.flist), \
+TEST_LIB_C_FILES := $(call filelist,tests/testlib.flist)
+
+$(foreach TEST,$(TEST_LIB_C_FILES), \
     $(eval TEST_LIB_NAME := $(call product_create,BINEXEC,$(notdir $(TEST:.c=)))) \
     $(eval TEST_LIB_TARGET := $(call product_target,$(TEST_LIB_NAME))) \
     $(eval $(TEST_LIB_TARGET): $(call bin_object_files,$(TEST))) \
+    $(eval $(TEST_LIB_TARGET): LDFLAGS += $(call bin_object_files,$(TEST))) \
     $(eval TEST_LIB_TARGETS += $(TEST_LIB_TARGET)) \
     $(eval $($(call test_product,$(TEST_LIB_NAME)))) \
 )
 
-$(TEST_LIB_TARGETS): $(LIB_HEADERS_TARGET)
+# ------------------------------------------------------------ C configuration
+$(TEST_LIB_TARGETS): CDEPS += $(LIB_HEADERS_TARGET)
+$(TEST_LIB_TARGETS): CFLAGS += -I $(LIB_HEADERS_TARGET) -I $(test_apis_dir) -g
+
+# ------------------------------------------------------------ link configuration
 $(TEST_LIB_TARGETS): $(LIB_BINARIES_TARGET)
-$(TEST_LIB_TARGETS): C_FLAGS += -I $(LIB_HEADERS_TARGET) -I $(test_apis_dir) -g
+$(TEST_LIB_TARGETS): LDFLAGS += $(LIB_BINARIES_TARGET)
 
