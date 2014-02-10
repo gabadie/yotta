81987162091a6a6b9e419c40353ec11b3084a72d
Guillaume Abadie <guillaume.abadie@gmail.com>
2014-02-10 16:35:20 +0100

    Implements global var yotta_executable_path

--- src/yotta_init.c
+++ src/yotta_init.c
@@ -1,8 +1,15 @@
 
 #include "yotta_init.h"
+#include "yotta_address_translation.h"
+
+char const *
+yotta_executable_path = 0;
+
 
 uint64_t
-yotta_init(uint64_t argc __attribute__((unused)), char ** argv __attribute__((unused)))
+yotta_init(uint64_t argc __attribute__((unused)), char const * const * argv)
 {
+    yotta_executable_path = argv[0];
+
     return 0;
 }
