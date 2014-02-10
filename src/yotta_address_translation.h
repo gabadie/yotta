a30ebdeca0a345f3b9e3367e7e593fc8eb40b283
Thierry CANTENOT <thierry.cantenot@insa-lyon.fr>
2014-02-10 16:31:17 +0100

    Added yotta_address_translation to flist

--- src/yotta_address_translation.h
+++ src/yotta_address_translation.h
@@ -1,5 +1,29 @@
-#include <inttypes.h>
+#ifndef _YOTTA_ADDRESS_TRANSLATION
+#define _YOTTA_ADDRESS_TRANSLATION
 
-uint64_t yotta_address_absolute_to_relative(uint64_t offset);
+#include "yotta_prefix.h"
 
-uint64_t yotta_address_relative_to_absolute(uint64_t offset);
+
+/*
+ * @infos: convert an absolute address to a relative one
+ *
+ * @param <address>: absolute address to convert
+ *
+ * @returns:
+ *  The relative address corresponding to the given absolute address
+ */
+uint64_t 
+yotta_address_absolute_to_relative(uint64_t address);
+
+/*
+ * @infos: convert an (function) offset to its absolute address
+ *
+ * @param <offset>: offset to convert
+ *
+ * @returns:
+ *  The absolute address corresponding to the given offset
+ */
+uint64_t
+yotta_address_relative_to_absolute(uint64_t offset);
+
+#endif //_YOTTA_ADDRESS_TRANSLATION
