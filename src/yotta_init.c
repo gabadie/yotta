83ef9c3eea0264daa0352de4ac39aee7720d0d5f
Guillaume Abadie <guillaume.abadie@gmail.com>
2014-02-10 20:00:13 +0100

    Lets yotta_init() call yotta_init_slave()

--- src/yotta_init.c
+++ src/yotta_init.c
@@ -1,3 +1,5 @@
+#include <string.h>
+#include <stdlib.h>
 
 #include "yotta_debug.h"
 #include "yotta_init.h"
@@ -8,8 +10,12 @@ char const *
 yotta_executable_path = 0;
 
 
+static uint64_t
+yotta_init_slave(uint64_t argc, char const * const * argv);
+
+
 uint64_t
-yotta_init(uint64_t argc __attribute__((unused)), char const * const * argv)
+yotta_init(uint64_t argc, char const * const * argv)
 {
     yotta_assert(argc > 1);
     yotta_assert(argv != 0);
@@ -20,6 +26,25 @@ yotta_init(uint64_t argc __attribute__((unused)), char const * const * argv)
     {
         return -1;
     }
+
+    for (uint64_t i = 1; i < argc; i++)
+    {
+        yotta_assert(argv[i] != 0);
+
+        if (strcmp(argv[i], "--yotta") == 0)
+        {
+            uint64_t returned = yotta_init_slave(argc - i - 1, argv + i + 1);
+
+            exit((int)returned);
+        }
+    }
+
+    return 0;
+}
+
+uint64_t
+yotta_init_slave(uint64_t argc __attribute__((unused)), char const * const * argv __attribute__((unused)))
+{
 
     return 0;
 }
