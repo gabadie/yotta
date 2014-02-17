995f3a2bceb4953ddd8c925bae8df3d3b3201718
Guillaume Abadie <guillaume.abadie@gmail.com>
2014-02-17 17:52:51 +0100

    Implements yotta_file_size()'s unit tests

---
+++ tests/testlib_file.c
@@ -0,0 +1,41 @@
+
+#include <mk_test.h>
+#include <stdio.h>
+
+#include "testlib_helpers.h"
+#include "../src/utils/yotta_file.h"
+
+
+void
+test_file_size()
+{
+    FILE * file = fopen(__FILE__, "rb");
+
+    {
+        test_assert(yotta_file_pos(file) == 0);
+        test_assert(yotta_file_size(file) != 0);
+        test_assert(yotta_file_pos(file) == 0);
+    }
+
+    {
+        const size_t pos = 12;
+
+        fseek(file, pos, SEEK_CUR);
+
+        test_assert(yotta_file_pos(file) == pos);
+        test_assert(yotta_file_size(file) != 0);
+        test_assert(yotta_file_pos(file) == pos);
+    }
+
+    fclose(file);
+}
+
+int
+main()
+{
+    yotta_test_helper_setup();
+
+    test_file_size();
+
+    return 0;
+}
