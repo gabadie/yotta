e722153f60604470493fb23183e59e5d335fd869
Guillaume Abadie <guillaume.abadie@gmail.com>
2014-02-21 15:15:36 +0100

    Creates the src/whisper/ directory

---
+++ src/whisper/yotta_whisper_pull.c
@@ -0,0 +1,26 @@
+
+#include "yotta_whisper_labels.private.h"
+
+void
+yotta_whisper_entry_pull_answer(
+    yotta_context_t * context,
+    yotta_socket_t * socket,
+    yotta_whisper_entry_feedback_t * feedback
+)
+{
+    (void) context;
+    (void) socket;
+    (void) feedback;
+}
+
+void
+yotta_whisper_entry_pull_request(
+    yotta_context_t * context,
+    yotta_socket_t * socket,
+    yotta_whisper_entry_feedback_t * feedback
+)
+{
+    (void) context;
+    (void) socket;
+    (void) feedback;
+}
---
+++ src/whisper/yotta_whisper_push.c
@@ -0,0 +1,15 @@
+
+#include "yotta_whisper_labels.private.h"
+
+
+void
+yotta_whisper_entry_push(
+    yotta_context_t * context,
+    yotta_socket_t * socket,
+    yotta_whisper_entry_feedback_t * feedback
+)
+{
+    (void) context;
+    (void) socket;
+    (void) feedback;
+}
