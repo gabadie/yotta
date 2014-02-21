339b2e6536a00b280a770a3fc7f92923dc0a57d7
Guillaume Abadie <guillaume.abadie@gmail.com>
2014-02-21 12:27:42 +0100

    Implements yotta_command_init()

---
+++ src/yotta_command.c
@@ -0,0 +1,36 @@
+
+#include <string.h>
+
+#include "yotta_command.h"
+#include "yotta_return.private.h"
+
+
+uint64_t
+yotta_command_init(yotta_command_t * command)
+{
+    if (command == 0)
+    {
+        yotta_return_inv_value(yotta_command_init, command);
+    }
+
+    memset(command, 0, sizeof(yotta_command_t));
+
+    return YOTTA_SUCCESS;
+}
+
+uint64_t
+yotta_command(const yotta_context_t * context, const yotta_command_t * command)
+{
+    if (context == 0)
+    {
+        yotta_return_inv_value(yotta_command, context);
+    }
+    else if (command == 0)
+    {
+        yotta_return_inv_value(yotta_command, command);
+    }
+
+    // TODO: code the content
+
+    return YOTTA_SUCCESS;
+}
