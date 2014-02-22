6ecb2fa65d958bbcdf0ed3ef26a37a7c2983ffbd
Guillaume Abadie <guillaume.abadie@gmail.com>
2014-02-22 23:39:42 +0100

    Implements testhelper_lorem()

---
+++ tests/testhelper_lorem.h
@@ -0,0 +1,22 @@
+#ifndef _TESTHELPER_LOREM
+#define _TESTHELPER_LOREM
+
+#include <stdlib.h>
+
+
+static
+void
+testhelper_lorem(void * data_dest, uint64_t data_size)
+{
+    uint32_t * cursor = (uint32_t *) data_dest;
+    uint32_t * cursor_end = (uint32_t *)(((uint8_t *) cursor) + data_size);
+
+    while(cursor < cursor_end)
+    {
+        *cursor = (uint32_t) rand();
+        cursor++;
+    }
+}
+
+
+#endif _TESTHELPER_LOREM
