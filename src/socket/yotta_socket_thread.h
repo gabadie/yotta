ec5bf1cdb43a38e1f18272a7cd82e988a8be8d0e
Thierry CANTENOT <thierry.cantenot@insa-lyon.fr>
2014-02-24 14:53:46 +0100

    Added socket thread

---
+++ src/socket/yotta_socket_thread.h
@@ -0,0 +1,11 @@
+#ifndef _YOTTA_SOCKET_THREAD
+#define _YOTTA_SOCKET_THREAD
+
+typedef struct
+yotta_socket_thread_s
+{
+    //yotta_socket_event_t * socket_events;
+
+} yotta_socket_thread_t;
+
+#endif //_YOTTA_SOCKET_THREAD
