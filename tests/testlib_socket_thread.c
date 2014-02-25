0302e03b9e0a1330cfe86a90996f30acc9c13b23
Guillaume Abadie <guillaume.abadie@gmail.com>
2014-02-25 22:01:49 +0100

    Tests yotta_socket_thread_kill()

--- tests/testlib_socket_thread.c
+++ tests/testlib_socket_thread.c
@@ -11,10 +11,17 @@ test_socket_thread_destroy()
     testhelper_whisper_protocol_t protocol;
 
     testhelper_whisper_protocol_init(&protocol);
-    {
-
-    }
     testhelper_whisper_protocol_destroy(&protocol);
+}
+
+static
+void
+test_socket_thread_kill()
+{
+    testhelper_whisper_protocol_t protocol;
+
+    testhelper_whisper_protocol_init(&protocol);
+    yotta_socket_thread_kill(&protocol.thread);
 }
 
 int
@@ -23,6 +30,7 @@ main()
     testhelper_init();
 
     test_socket_thread_destroy();
+    test_socket_thread_kill();
 
     return 0;
 }
