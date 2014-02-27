0189a154474af304171a4f7538510cf69b93fd7b
Guillaume Abadie <guillaume.abadie@gmail.com>
2014-02-27 16:41:57 +0100

    Migrates yotta_whisper_push() to the whisper protocol queue machanism

--- src/whisper/yotta_whisper_push.c
+++ src/whisper/yotta_whisper_push.c
@@ -3,6 +3,7 @@
 #include "yotta_whisper_labels.private.h"
 #include "../socket/yotta_tcp.h"
 #include "../yotta_debug.h"
+#include "../yotta_memory.h"
 
 
 void
@@ -82,6 +83,83 @@ yotta_whisper_entry_push(
     }
 }
 
+
+typedef struct
+yotta_whisper_push_header_s
+{
+    uint16_t label;
+    uint64_t master_address;
+    uint64_t data_size;
+} __attribute__((packed))
+yotta_whisper_push_header_t;
+
+typedef struct
+yotta_whisper_push_cmd_s
+{
+    yotta_tcp_cmd_t abstract_cmd;
+    yotta_whisper_push_header_t header;
+    uint64_t header_sent;
+    void const * data;
+    uint64_t data_sent;
+}
+yotta_whisper_push_cmd_t;
+
+static
+void
+yotta_whisper_push_send(yotta_whisper_push_cmd_t * cmd, yotta_tcp_queue_t * cmd_queue)
+{
+    if (cmd->header_sent != sizeof(yotta_whisper_push_header_t))
+    {
+        ssize_t header_sending = sizeof(yotta_whisper_push_header_t) - cmd->header_sent;
+
+        ssize_t header_sent = yotta_tcp_send(
+            (yotta_socket_t *) cmd_queue,
+            ((uint8_t *) &cmd->header) + cmd->header_sent,
+            header_sending
+        );
+
+        if (header_sent == -1)
+        {
+            return;
+        }
+
+        cmd->header_sent += (uint64_t)header_sending;
+
+        if (cmd->header_sent != sizeof(yotta_whisper_push_header_t))
+        {
+            return;
+        }
+    }
+
+    ssize_t data_sending = cmd->header.data_size - cmd->data_sent;
+
+    ssize_t data_sent = yotta_tcp_send(
+        (yotta_socket_t *) cmd_queue,
+        ((uint8_t *) cmd->data) + cmd->data_sent,
+        data_sending
+    );
+
+    if (data_sent == -1)
+    {
+        return;
+    }
+
+    cmd->data_sent += (uint64_t)data_sent;
+
+    if (cmd->data_sent == cmd->header.data_size)
+    {
+        yotta_tcp_cmd_dequeue(cmd_queue);
+        yotta_tcp_cmd_release(cmd);
+    }
+}
+
+static
+void
+yotta_whisper_push_release(yotta_tcp_cmd_t * cmd)
+{
+    yotta_free(cmd);
+}
+
 void
 yotta_whisper_push(
     yotta_whisper_queue_t * cmd_queue,
@@ -90,21 +168,22 @@ yotta_whisper_push(
     void const * data
 )
 {
-    struct
-    {
-        uint16_t label;
-        uint64_t master_address;
-        uint64_t data_size;
-    } __attribute__((packed)) header;
-
-    header.label = YOTTA_WHISPER_MEM_PUSH;
-    header.master_address = master_address;
-    header.data_size = data_size;
-
-    yotta_assert(socket != 0);
+    yotta_assert(cmd_queue != 0);
+    yotta_assert(master_address != 0);
     yotta_assert(data_size != 0);
     yotta_assert(data != 0);
 
-    yotta_tcp_send(&cmd_queue->tcp_queue.socket_event.socket, &header, sizeof(header));
-    yotta_tcp_send(&cmd_queue->tcp_queue.socket_event.socket, data, data_size);
+    yotta_whisper_push_cmd_t * cmd = yotta_alloc_s(yotta_whisper_push_cmd_t);
+
+    yotta_tcp_cmd_set_send(cmd, yotta_whisper_push_send);
+    yotta_tcp_cmd_set_release(cmd, yotta_whisper_push_release);
+
+    cmd->header.label = YOTTA_WHISPER_MEM_PUSH;
+    cmd->header.master_address = master_address;
+    cmd->header.data_size = data_size;
+    cmd->header_sent = 0;
+    cmd->data = data;
+    cmd->data_sent = 0;
+
+    yotta_tcp_queue_append(cmd_queue, cmd);
 }
--- tests/testlib_whisper_push.c
+++ tests/testlib_whisper_push.c
@@ -2,7 +2,8 @@
 #include <string.h>
 
 #include "testhelper_tcp_sockets.h"
-//#include "testhelper_lorem.h"
+#include "testhelper_whisper.h"
+#include "testhelper_lorem.h"
 #include "../src/whisper/yotta_whisper_push.private.h"
 #include "../src/whisper/yotta_whisper_labels.private.h"
 
@@ -19,6 +20,7 @@ testlib_whisper_push_frame_s
 } __attribute__((packed))
 testlib_whisper_push_frame_t;
 
+#if 0
 static
 void
 test_whisper_push()
@@ -62,6 +64,7 @@ test_whisper_push()
 
     testhelper_tcp_clean(&sockets);
 }
+#endif
 
 #if 0
 static
@@ -172,14 +175,35 @@ test_whisper_push_stress()
 
 #endif
 
+static
+void
+test_whisper_push_stress()
+{
+    static uint64_t const data_size = 1024 * 1024;// 1024 * 1024 * 1;
+    testhelper_whisper_protocol_t protocol;
+
+    void * src_data = malloc(data_size);
+    void * dest_data = malloc(data_size);
+
+    testhelper_whisper_protocol_init(&protocol);
+    testhelper_lorem(src_data, data_size);
+
+    yotta_whisper_push(&protocol.queue0, (uint64_t) dest_data, data_size, src_data);
+
+    testhelper_whisper_protocol_destroy(&protocol);
+
+    test_assert(memcmp(src_data, dest_data, data_size) == 0);
+
+    free(src_data);
+    free(dest_data);
+}
+
 int
 main()
 {
     testhelper_init();
 
-    test_whisper_push();
-    //test_whisper_push_entry();
-    //test_whisper_push_stress();
+    test_whisper_push_stress();
 
     return 0;
 }
