445e0996e8ee4b5ac109b033a6e004ff65420721
Guillaume Abadie <guillaume.abadie@gmail.com>
2014-02-27 23:25:13 +0100

    Adds assertions in src/whisper/yotta_whisper_master.c

--- src/whisper/yotta_whisper_master.c
+++ src/whisper/yotta_whisper_master.c
@@ -1,6 +1,8 @@
 
+
 #include "yotta_whisper_master.private.h"
 #include "yotta_whisper_queue.private.h"
+#include "../core/yotta_debug.h"
 #include "../core/yotta_logger.private.h"
 #include "../core/yotta_memory.h"
 #include "../socket/yotta_socket_thread.h"
@@ -11,6 +13,8 @@ static
 void
 yotta_whisper_queue_release(yotta_whisper_queue_t * cmd_queue)
 {
+    yotta_assert(cmd_queue != 0);
+
     yotta_whisper_queue_destroy(cmd_queue);
 
     yotta_free(cmd_queue);
@@ -20,14 +24,19 @@ static
 void
 yotta_whisper_master_recv(yotta_whisper_master_t * master)
 {
+    yotta_assert(master != 0);
+
     yotta_logger_debug("yotta_whisper_master_recv: new slave connection");
 
-    // TODO: yotta_whisper_queue
     yotta_socket_t slave_socket;
 
     yotta_whisper_queue_t * cmd_queue = yotta_alloc_s(yotta_whisper_queue_t);
 
-    yotta_accept_socket(&master->socket, &slave_socket);
+    if (yotta_accept_socket(&master->socket, &slave_socket) != 0)
+    {
+        yotta_logger_debug("yotta_whisper_master_recv: connection has failed");
+        return;
+    }
 
     yotta_whisper_queue_init(cmd_queue);
     yotta_socket_event_set_release(cmd_queue, yotta_whisper_queue_release);
@@ -43,6 +52,8 @@ static
 void
 yotta_whisper_master_except(yotta_whisper_master_t * master)
 {
+    yotta_assert(master != 0);
+
     yotta_logger_error("yotta_whisper_master_except: received a TCP socket exception -> releasing");
 
     yotta_socket_event_release(master);
@@ -51,6 +62,8 @@ yotta_whisper_master_except(yotta_whisper_master_t * master)
 uint64_t
 yotta_whisper_master_init(yotta_whisper_master_t * master, uint16_t listening_port)
 {
+    yotta_assert(master != 0);
+
     if (yotta_tcp_socket_server(&master->socket, listening_port) != 0)
     {
         return -1;
@@ -71,5 +84,7 @@ yotta_whisper_master_init(yotta_whisper_master_t * master, uint16_t listening_po
 uint64_t
 yotta_whisper_master_destroy(yotta_whisper_master_t * master)
 {
+    yotta_assert(master != 0);
+
     return yotta_socket_event_destroy(master);
 }
