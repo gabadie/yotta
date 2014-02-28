74309ad1dea6a4e055137df2520cdd4b806a9a95
Thierry CANTENOT <thierry.cantenot@insa-lyon.fr>
2014-02-28 13:37:19 +0100

    Added private semaphores pool

---
+++ src/yotta_sem.private.h
@@ -0,0 +1,33 @@
+#ifndef _YOTTA_SEM
+#define _YOTTA_SEM
+
+#include <semaphore.h>
+
+#include "yotta_prefix.h"
+
+/*
+ * @infos: get a POSIX semaphore from a semaphore pool
+ *
+ * @param <out_sem>: retrieved POSIX semaphore
+ *
+ * @returns:
+ *  == <0> if succeed
+ *  != <0> if failed
+ */
+uint64_t
+yotta_sem_fetch(sem_t ** out_sem);
+
+/*
+ * @infos: release a POSIX semaphore into the pool
+ *
+ * @param <sem>: POSIX semaphore to release
+ *
+ * @returns:
+ *  == <0> if succeed
+ *  != <0> if failed
+ */
+uint64_t
+yotta_sem_release(sem_t * sem);
+
+
+#endif //_YOTTA_SEM
