fc3a9a4ec3864e8baf9c72f0637a348875c014ee
Guillaume Abadie <guillaume.abadie@gmail.com>
2014-03-02 15:13:43 +0100

    Lets yotta_threading_cores() cache the number of available cores on Mac OS X

--- src/threading/yotta_threading.c
+++ src/threading/yotta_threading.c
@@ -10,24 +10,40 @@
 uint64_t
 yotta_threading_cores(uint64_t * cores)
 {
+    static int32_t cores_cache = 0;
+
+    if (cores_cache > 0)
+    {
+        *cores = (uint64_t) cores_cache;
+
+        return YOTTA_SUCCESS;
+    }
+
+    int32_t cores_returned = 0;
     int32_t mib[2];
-    size_t len = sizeof(*cores);
+    size_t len = sizeof(cores_returned);
 
     mib[0] = CTL_HW;
     mib[1] = HW_AVAILCPU;
 
-    sysctl(mib, 2, cores, &len, 0, 0);
+    sysctl(mib, 2, &cores_returned, &len, 0, 0);
 
-    if (*cores > 0)
+    if (cores_returned > 0)
     {
+        cores_cache = cores_returned;
+        *cores = (uint64_t) cores_cache;
+
         return YOTTA_SUCCESS;
     }
 
     mib[1] = HW_NCPU;
-    sysctl(mib, 2, cores, &len, 0, 0);
+    sysctl(mib, 2, &cores_returned, &len, 0, 0);
 
-    if (*cores > 0)
+    if (cores_returned > 0)
     {
+        cores_cache = cores_returned;
+        *cores = (uint64_t) cores_cache;
+
         return YOTTA_SUCCESS;
     }
 
