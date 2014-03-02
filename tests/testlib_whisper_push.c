faa57805011bf70d81ccd6802ef9376e4fb0d1b0
Guillaume Abadie <guillaume.abadie@gmail.com>
2014-03-02 10:58:13 +0100

    Tests yotta_whisper_push()'s sync_finished parameter

--- tests/testlib_whisper_push.c
+++ tests/testlib_whisper_push.c
@@ -31,6 +31,37 @@ test_whisper_push_stress()
     testhelper_memory_check();
 }
 
+static
+void
+test_whisper_push_sync()
+{
+    static uint64_t const data_size = 1024 * 1024;
+    testhelper_whisper_protocol_t protocol;
+
+    void * src_data = malloc(data_size);
+    void * dest_data = malloc(data_size);
+
+    testhelper_whisper_protocol_init(&protocol);
+    testhelper_lorem(src_data, data_size);
+
+    {
+        yotta_sync_t sync_push;
+
+        yotta_whisper_push(&protocol.queue0, (uint64_t) dest_data, data_size, src_data, &sync_push);
+        yotta_sync_wait(&sync_push);
+    }
+
+    test_assert(memcmp(src_data, dest_data, data_size) == 0);
+
+    free(src_data);
+    free(dest_data);
+
+    testhelper_whisper_protocol_destroy(&protocol);
+
+    // TODO: implements yotta_semaphore_pool_flush()
+    //testhelper_memory_check();
+}
+
 int
 main()
 {
@@ -38,6 +69,7 @@ main()
     testhelper_memory_setup();
 
     test_whisper_push_stress();
+    test_whisper_push_sync();
 
     return 0;
 }
