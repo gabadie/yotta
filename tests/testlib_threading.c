a928d61ec55122f732e725e2e838a77eb9cd9a75
Thierry CANTENOT <thierry.cantenot@insa-lyon.fr>
2014-03-03 10:48:50 +0100

    Fixed uint64_t print

--- tests/testlib_threading.c
+++ tests/testlib_threading.c
@@ -3,6 +3,9 @@
 #include <yotta.h>
 #include <mk_test.h>
 
+#define __STDC_FORMAT_MACROS
+#include <inttypes.h>
+#undef __STDC_FORMAT_MACROS
 
 void
 test_threading_cores()
@@ -12,7 +15,7 @@ test_threading_cores()
     test_assert(yotta_threading_cores(&cores) == 0);
     test_assert(cores >  0);
 
-    printf("number of cores: %llu\n", cores);
+    printf("number of cores: %" PRIu64 "\n", cores);
 }
 
 int
