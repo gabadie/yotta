c26745bce95575f7741cd53d00e9441ced829797
Guillaume Abadie <guillaume.abadie@gmail.com>
2014-03-04 15:08:22 +0100

    Implements yotta_threading_{tid,pid}()

--- src/threading/yotta_threading.h
+++ src/threading/yotta_threading.h
@@ -4,6 +4,17 @@
 #include "../yotta_prefix.h"
 
 
+#if defined(_WIN32) || defined(_WIN64)
+#error "yotta_threading.h not suported on Windows yet"
+
+#else
+#include <sys/types.h>
+#include <unistd.h>
+#include <pthread.h>
+
+#endif
+
+
 /*
  * @infos: gets the number of availables core
  *
@@ -11,6 +22,22 @@
  */
 yotta_return_t
 yotta_threading_cores();
+
+/*
+ * @infos: gets the process's ID
+ *
+ * @returns: (uint64_t) the process's ID
+ */
+#define yotta_threading_pid() \
+    (uint64_t) getpid();
+
+/*
+ * @infos: gets the thread's ID
+ *
+ * @returns: (uint64_t) the thread's ID
+ */
+#define yotta_threading_tid() \
+    (uint64_t) pthread_self();
 
 
 #endif //_YOTTA_THREADING
