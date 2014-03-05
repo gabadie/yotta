7b1022f4d7b06e2f6aa8889060179dd1b588eddd
Guillaume Abadie <guillaume.abadie@gmail.com>
2014-03-05 05:49:45 +0100

    Implements YOTTA_{LINUX,OSX,UNIX,WINDOWS} macros

--- src/yotta_prefix.h
+++ src/yotta_prefix.h
@@ -4,6 +4,23 @@
 #include <stdint.h>
 #include <stdlib.h>
 
+/*
+ * Defines compilation environment YOTTA_{LINUX,OSX,UNIX,WINDOWS}
+ */
+#ifdef __APPLE__
+#define YOTTA_OSX
+#define YOTTA_UNIX
+#endif
+
+#ifdef __linux__
+#define YOTTA_LINUX
+#define YOTTA_UNIX
+#endif
+
+#if defined(_WIN32) || defined(_WIN64)
+#define YOTTA_WINDOWS
+#endif
+
 #include "core/yotta_return.h"
 
 
