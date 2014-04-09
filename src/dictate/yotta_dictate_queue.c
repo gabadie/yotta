eff0590ebab48f8e12a71553dfc4d1dfd3b1ef69
Thierry CANTENOT <thierry.cantenot@insa-lyon.fr>
2014-04-09 10:17:46 +0200

    Added yotta_dictate_queue

---
+++ src/dictate/yotta_dictate_queue.c
@@ -0,0 +1,163 @@
+#include "yotta_dictate_queue.private.h"
+#include "yotta_dictate_labels.private.h"
+
+
+#ifdef YOTTA_DEBUG
+
+/*
+ * @infos: verify that the receive buffer has been cleaned up
+ *
+ * @param <cmd_queue>: the command queue to verify
+ */
+static
+void
+yotta_dictate_queue_assert_buffer(yotta_dictate_queue_t * cmd_queue)
+{
+    yotta_assert(cmd_queue != 0);
+
+    for (size_t i = 0; i < sizeof(cmd_queue->recv_buffer); i++)
+    {
+        yotta_assert(cmd_queue->recv_buffer[i] == 0);
+    }
+}
+
+#endif //YOTTA_DEBUG
+
+
+static
+void
+yotta_dictate_queue_recv(yotta_dictate_queue_t * cmd_queue)
+{
+    yotta_assert(cmd_queue != NULL);
+
+    while (1)
+    {
+        if (cmd_queue->callback != NULL)
+        {
+            cmd_queue->callback(cmd_queue);
+
+            if (cmd_queue->callback != NULL)
+            {
+                // the callback has not finished to receive all data
+                break;
+            }
+
+#ifdef YOTTA_DEBUG
+            yotta_dictate_queue_assert_buffer(cmd_queue);
+#endif //YOTTA_DEBUG
+        }
+
+        yotta_dictate_label_t label = 0x0000;
+
+        ssize_t label_size = yotta_tcp_recv(&cmd_queue->tcp_queue.socket_event.socket, &label, sizeof(label));
+
+        if (label_size == sizeof(label))
+        {
+            switch (label)
+            {
+                case YOTTA_DICTATE_LABEL_DEAMON_INFO:
+                {
+                    cmd_queue->callback = yotta_dictate_daemon_info_recv;
+                    break;
+                }
+
+                case YOTTA_DICTATE_LABEL_ERROR:
+                {
+                    cmd_queue->callback = yotta_dictate_error_recv;
+                    break;
+                }
+
+                default:
+                {
+                    cmd_queue->callback = yotta_dictate_unknown_label;
+                }
+            }
+
+            yotta_assert(cmd_queue->callback != 0);
+
+            continue;
+        }
+        else if (label_size == 0)
+        {
+            // the socket has been closed properly on the other side
+
+            yotta_socket_event_unlisten((yotta_socket_event_t *) cmd_queue);
+            yotta_socket_event_release(cmd_queue);
+
+            return;
+        }
+        else if (label_size == -1)
+        {
+            int32_t errno_recv = errno;
+
+            if (errno_recv == EAGAIN)
+            {
+                // no incomming information available
+                break;
+            }
+        }
+
+        yotta_crash_msg("receive error");
+    }
+}
+
+static
+void
+yotta_dictate_queue_except(yotta_dictate_queue_t * cmd_queue)
+{
+    yotta_assert(cmd_queue != 0);
+
+    yotta_logger_error("yotta_dictate_queue_except: received a TCP socket exception -> releasing");
+
+    yotta_socket_event_unlisten((yotta_socket_event_t *) cmd_queue);
+
+    yotta_socket_event_release(cmd_queue);
+}
+
+void
+yotta_dictate_queue_init(yotta_dictate_queue_t * cmd_queue)
+{
+    yotta_assert(cmd_queue != NULL);
+    yotta_assert(sizeof(yotta_dictate_label_t) == 2);
+
+    yotta_dirty_offset(
+        cmd_queue,
+        sizeof(cmd_queue->tcp_queue),
+        sizeof(*cmd_queue) - sizeof(cmd_queue->tcp_queue)
+    );
+
+    yotta_tcp_queue_init((yotta_tcp_queue_t *) cmd_queue);
+    yotta_socket_event_set_recv(cmd_queue, yotta_dictate_queue_recv);
+    yotta_socket_event_set_except(cmd_queue, yotta_dictate_queue_except);
+
+    cmd_queue->callback = NULL;
+
+    memset(&cmd_queue->recv_buffer, 0, YOTTA_DICTATE_RECV_BUFFER_SIZE);
+}
+
+yotta_return_t
+yotta_dictate_queue_connect(yotta_dictate_queue_t * cmd_queue, char const * ip, uint16_t port)
+{
+    yotta_assert(cmd_queue != NULL);
+    yotta_assert(ip != NULL);
+    yotta_assert(port != 0);
+
+    yotta_dirty_s(cmd_queue);
+
+    if (yotta_tcp_socket_client((yotta_socket_t *) cmd_queue, ip, port) != 0)
+    {
+        return YOTTA_UNEXPECTED_FAIL;
+    }
+
+    yotta_dictate_queue_init(cmd_queue);
+
+    return YOTTA_SUCCESS;
+}
+
+void
+yotta_dictate_queue_destroy(yotta_dictate_queue_t * cmd_queue)
+{
+    yotta_assert(cmd_queue != NULL);
+
+    yotta_tcp_queue_destroy((yotta_tcp_queue_t *) cmd_queue);
+}
---
+++ src/dictate/yotta_dictate_queue.private.h
@@ -0,0 +1,95 @@
+#ifndef _YOTTAPRIVATE_DICTATE_QUEUE
+#define _YOTTAPRIVATE_DICTATE_QUEUE
+
+#include "../socket/yotta_tcp_queue.private.h"
+
+/*
+ * @infos: defines the sizes of the receive buffer in bytes
+ */
+#define YOTTA_DICTATE_RECV_BUFFER_SIZE 512
+
+/*
+ *@infos: yotta_dictate_queue struct's forward declaration
+ */
+typedef struct
+yotta_dictate_queue_s
+yotta_dictate_queue_t;
+
+/*
+ * @infos: defines the yotta dictate entry pointer type
+ *
+ * The entry receive 1 parameter:
+ *  - yotta_dictate_queue_t * <cmd_queue>: the receiving command queue
+ */
+typedef void (* yotta_dictate_recv_t)(
+    yotta_dictate_queue_t *
+);
+
+/*
+ * @infos: defines a dictate commands queue
+ */
+struct
+yotta_dictate_queue_s
+{
+    // Inheritance from TCP queue
+    yotta_tcp_queue_t tcp_queue;
+
+    // callback entry to complete the message
+    yotta_dictate_recv_t callback;
+
+    // temporary buffer
+    uint8_t recv_buffer[YOTTA_DICTATE_RECV_BUFFER_SIZE];
+};
+
+/*
+ * @infos: initializes a dictate command queue
+ *
+ * @param <cmd_queue>: the command queue to initialize
+ */
+void
+yotta_dictate_queue_init(yotta_dictate_queue_t * cmd_queue);
+
+/*
+ * @infos: connects a dictate command queue to a dictate master
+ *
+ * @param <cmd_queue>: the command queue to connect to
+ * @param <ip>: the dictate master' ip
+ * @param <port>: the dictate master' port
+ *
+ * @returns:
+ *  == <0> if succeed
+ *  != <0> if failed
+ */
+yotta_return_t
+yotta_dictate_queue_connect(yotta_dictate_queue_t * cmd_queue, char const * ip, uint16_t port);
+
+/*
+ * @infos: destroyes a dictate command queue
+ *
+ * @param <cmd_queue>: the command queue to destroy
+ */
+void
+yotta_dictate_queue_destroy(yotta_dictate_queue_t * cmd_queue);
+
+/*
+ * @infos: gets the receive buffer
+ *
+ * @param <cmd_queue>: the command queue
+ * @param <buffer_type>: the receive buffer struct to cast to
+ *
+ * @return:
+ *  (<buffer_type> *) receive buffer
+ */
+#define yotta_dictate_queue_recv_buffer(cmd_queue,buffer_type) \
+    (buffer_type *) (cmd_queue)->recv_buffer ; \
+    yotta_assert(sizeof(buffer_type) <= YOTTA_DICTATE_RECV_BUFFER_SIZE)
+
+/*
+ * @infos: finish  in the <cmd_queue> the number of unreaded bytes
+ *
+ * @param <cmd_queue>: the command queue
+ */
+#define yotta_dictate_queue_finish(cmd_queue) \
+    (cmd_queue)->callback = 0;
+
+#endif
