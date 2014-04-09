ef8882536d5a340b8c5ac92bf476a793278263da
Guillaume Abadie <guillaume.abadie@gmail.com>
2014-04-09 09:55:05 +0200

    Implements deamon.Deamon class

--- src_deamon/dictate.py
+++ src_deamon/dictate.py
@@ -1,13 +1,14 @@
 
 from twisted.internet.protocol import Protocol
 from twisted.internet.protocol import Factory
-from twisted.internet.endpoints import TCP4ServerEndpoint
-from twisted.internet import reactor
+
 from ctypes import *
+
+# struct.pack("qQ", hello)
 
 
 class deamon_infos():
-    def __init__(self): 
+    def __init__(self):
         self.data_label = c_uint64(0x1000)
         self.data_size = c_ulonglong(16)
         # number of available computers
@@ -22,9 +23,9 @@ class DeamonProtocol(Protocol):
 
     def connectionMade(self):
         self.transport.write("Connection Made\r\n")
-        
+
     def dataReceived(self, data):
-        if self.state == "WAITING": 
+        if self.state == "WAITING":
             datasent = deamon_infos()
             self.sendInformation(datasent)
             self.state = "READY"
@@ -38,18 +39,6 @@ class DeamonProtocol(Protocol):
     def sendInformation(self, data):
         self.transport.write(struct.pack("!L",data.data_label))
 
-
-
-
 class DeamonProtocolFactory(Factory):
     def buildProtocol(self,addr):
         return DeamonProtocol()
-
-
-def main():
-    endpoint = TCP4ServerEndpoint(reactor,8037)
-    endpoint.listen(DeamonProtocolFactory())
-    reactor.run()
-
-if __name__ == "__main__":
-    main()
