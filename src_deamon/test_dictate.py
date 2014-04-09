16c301791b24d22af9a3956d7e2edefd62ae437e
Guillaume Abadie <guillaume.abadie@gmail.com>
2014-04-10 00:29:27 +0200

    Tests dictate.frame_error()

--- src_deamon/test_dictate.py
+++ src_deamon/test_dictate.py
@@ -4,17 +4,30 @@ import struct
 from deamon import Deamon
 import dictate
 
+
+def test_frame_error():
+    msg = 'hello world'
+
+    frame = dictate.frame_error(msg)
+
+    theoric_frame = ''
+    theoric_frame += '\x00\x00'
+    theoric_frame += '\x0B\x00\x00\x00\x00\x00\x00\x00'
+    theoric_frame += struct.pack('s', msg)
+
+    assert frame == theoric_frame
+
 def test_deamon_info():
     deamon = Deamon()
     deamon.computers = 3
     deamon.threads = 7
 
-    deamon_info_tram = dictate.deamon_info(deamon)
+    frame = dictate.deamon_info(deamon)
 
-    theoric_tram = ''
-    theoric_tram += '\x00\x10'
-    theoric_tram += '\x10\x00\x00\x00\x00\x00\x00\x00'
-    theoric_tram += '\x03\x00\x00\x00\x00\x00\x00\x00'
-    theoric_tram += '\x07\x00\x00\x00\x00\x00\x00\x00'
+    theoric_frame = ''
+    theoric_frame += '\x00\x10'
+    theoric_frame += '\x10\x00\x00\x00\x00\x00\x00\x00'
+    theoric_frame += '\x03\x00\x00\x00\x00\x00\x00\x00'
+    theoric_frame += '\x07\x00\x00\x00\x00\x00\x00\x00'
 
-    assert deamon_info_tram == theoric_tram
+    assert frame == theoric_frame
