73435bf157a882a3a78153e7358645b6537c7ec3
Guillaume Abadie <guillaume.abadie@gmail.com>
2014-04-09 10:03:28 +0200

    Implements deamon.Deamon.tcp_endpoints property

--- src_deamon/deamon.py
+++ src_deamon/deamon.py
@@ -12,16 +12,36 @@ class Deamon(object):
     members:
         computers = int
         threads = int
+        listen_port = int
+        tcp4_endpoint = twisted.internet.endpoints.TCP4ServerEndpoint
+        tcp6_endpoint = twisted.internet.endpoints.TCP6ServerEndpoint
+        dictate_factory = dictate.DeamonProtocolFactory
     """
 
-    def __init__(self):
+    def __init__(self, listen_port=5000):
         self.computers = 1
         self.threads = 1
+        self.listen_port = listen_port
+        self.tcp4_endpoint = TCP4ServerEndpoint(reactor, self.listen_port)
+        self.tcp6_endpoint = None
+        self.dictate_factory = dictate.DeamonProtocolFactory()
+
+        for endpoint in self.tcp_endpoints:
+            endpoint.listen(self.dictate_factory)
+
+    @property
+    def tcp_endpoints(self):
+        endpoints = []
+
+        if self.tcp4_endpoint:
+            endpoints.append(self.tcp4_endpoint)
+
+        if self.tcp6_endpoint:
+            endpoints.append(self.tcp6_endpoint)
+
+        return endpoints
 
     def main(self):
-        endpoint = TCP4ServerEndpoint(reactor,8037)
-        endpoint.listen(dictate.DeamonProtocolFactory())
-
         reactor.run()
 
         return 0
