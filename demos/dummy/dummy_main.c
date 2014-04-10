3ab93b0ac1e1fd1a9922f78d97b2498e73a18d51
Guillaume Abadie <guillaume.abadie@gmail.com>
2014-04-10 13:00:32 +0200

    Completes demos/dummy/dummy_main.c

--- demos/dummy/dummy_main.c
+++ demos/dummy/dummy_main.c
@@ -2,10 +2,35 @@
 #include <yotta.h>
 
 
+void
+launch(yotta_context_t * context)
+{
+    (void) context;
+}
+
 int
 main(int argc, char const * const * argv)
 {
     yotta_init(argc, argv);
+
+    yotta_context_t context;
+
+    if (yotta_context_init(&context, 0))
+    {
+        return 1;
+    }
+
+    if (yotta_context_connect(&context, "127.0.0.1", 5000))
+    {
+        return 1;
+    }
+
+    launch(&context);
+
+    if (yotta_context_destroy(&context))
+    {
+        return 1;
+    }
 
     return 0;
 }
