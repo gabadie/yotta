60c98c8b4ebe9fbcf704c1a24bae0663cdafaafe
Thierry CANTENOT <thierry.cantenot@insa-lyon.fr>
2014-04-15 22:59:27 +0200

    Updated demo

--- demos/dummy/dummy_main.c
+++ demos/dummy/dummy_main.c
@@ -1,9 +1,7 @@
 
 #include <inttypes.h>
-#include <math.h>
 #include <stdio.h>
 #include <yotta.h>
-
 
 static
 void
@@ -14,23 +12,41 @@ logger(void * user_data, yotta_logger_param_t const * param)
     fprintf(stderr, "%s\n", param->msg);
 }
 
+typedef
+struct prime_s
+{
+    uint64_t nb;
+    yotta_addr_t master_addr;
+}
+prime_t;
+
+
 static
 void
-is_prime(void * nb)
+is_prime(prime_t * prime)
 {
-    fprintf(stderr, "In is_prime\n");
-    uint64_t sqrt_nb = sqrt((uint64_t)nb) + 1;
+    uint64_t nb = prime->nb;
+    fprintf(stderr, "In is_prime %" PRIu64 "\n", nb);
 
-    for (uint64_t i = 0; i < sqrt_nb; i++)
+    uint8_t res = 1;
+
+    for (uint64_t i = 2; i * i < nb; i++)
     {
-        if(((uint64_t)nb) % i == 0)
+        if (nb % i == 0)
         {
-            fprintf(stderr, "%" PRIu64 " is not prime\n", (uint64_t) nb);
-            return;
+            fprintf(stderr, "%" PRIu64 " is not prime\n", nb);
+            res = 0;
+            break;
         }
     }
 
-    fprintf(stderr, "%" PRIu64 " is prime\n", (uint64_t) nb);
+    fprintf(stderr, "%" PRIu64 " is prime\n", nb);
+
+    yotta_sync_t sync;
+    yotta_push_package(prime->master_addr, 1, &res, &sync);
+    yotta_sync_wait(&sync);
+
+    fprintf(stderr, "Push DONE\n");
 }
 
 int
@@ -55,14 +71,22 @@ main(int argc, char const * const * argv)
 
     fprintf(stderr, "Connected\n");
 
-    uint64_t nb = 43;
+    uint8_t result = 2;
 
-    if (yotta_context_massive(&context, is_prime, 8, &nb, 0))
+    prime_t prime;
+    prime.nb = 43;
+    prime.master_addr = yotta_addr(&result);
+
+    /*is_prime(&prime);*/
+
+    if (yotta_context_massive(&context, (yotta_massive_command_entry_t) is_prime, 8, &prime, 0))
     {
         return 1;
     }
 
     fprintf(stderr, "Massived\n");
+
+    fprintf(stderr, "%" PRIu64 " is prime ? %d\n", prime.nb, result);
 
     if (yotta_context_destroy(&context))
     {
