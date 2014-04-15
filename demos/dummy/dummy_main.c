5d71d2fb6991661d55df773d3a465d85037240c5
Thierry CANTENOT <thierry.cantenot@insa-lyon.fr>
2014-04-16 00:44:12 +0200

    Updated dummy demo

--- demos/dummy/dummy_main.c
+++ demos/dummy/dummy_main.c
@@ -3,6 +3,8 @@
 #include <stdio.h>
 #include <yotta.h>
 
+#define RANGE_SIZE 10
+
 static
 void
 logger(void * user_data, yotta_logger_param_t const * param)
@@ -16,37 +18,111 @@ typedef
 struct prime_s
 {
     uint64_t nb;
+    uint64_t range;
     yotta_addr_t master_addr;
 }
 prime_t;
 
+static inline
+int
+is_prime(uint64_t nb)
+{
+    if(nb < 2) return 0;
+    for (size_t k = 2; k * k <= nb; k++)
+        if (nb % k == 0) return 0;
+    return 1;
+}
 
 static
 void
-is_prime(prime_t * prime)
+is_prime_range(prime_t * prime)
 {
-    uint64_t nb = prime->nb;
-    fprintf(stderr, "In is_prime %" PRIu64 "\n", nb);
+    uint8_t * results = yotta_alloc_sa(uint8_t, prime->range);
+    memset(results, 0, prime->range);
 
-    uint8_t res = 1;
-
-    for (uint64_t i = 2; i * i < nb; i++)
+    for(uint64_t i = 0; i < prime->range; i++)
     {
-        if (nb % i == 0)
+        for (uint64_t j = 0; j < 8; j++)
         {
-            fprintf(stderr, "%" PRIu64 " is not prime\n", nb);
-            res = 0;
-            break;
+            uint64_t nb = prime->nb + i * 8 + j;
+
+            if(is_prime(nb))
+            {
+                results[i] |= (1u << j);
+                fprintf(stderr, "%" PRIu64 " is prime\n", nb);
+            }
+            else
+            {
+                fprintf(stderr, "%" PRIu64 " is not prime\n", nb);
+            }
         }
+        fprintf(stderr, "%d\n", results[i]);
     }
 
-    fprintf(stderr, "%" PRIu64 " is prime\n", nb);
-
+    // Send the results to the master
     yotta_sync_t sync;
-    yotta_push_package(prime->master_addr, 1, &res, &sync);
+    yotta_push_package(prime->master_addr, prime->range, results, &sync);
     yotta_sync_wait(&sync);
 
     fprintf(stderr, "Push DONE\n");
+
+    yotta_free(results);
+}
+
+typedef
+struct thread_prime_s
+{
+    prime_t * prime;
+    uint8_t * results;
+}
+thread_prime_t;
+
+static
+void
+thread_is_prime(thread_prime_t * thread_prime)
+{
+    uint64_t tid = 0;
+    uint64_t nb_threads = 0;
+    prime_t * prime = thread_prime->prime;
+
+    yotta_get_local_id(&tid, &nb_threads);
+
+    /*fprintf(stderr, "In thread %" PRIu64 " (%" PRIu64 " threads)\n", tid, nb_threads);*/
+
+    for (uint64_t j = tid; j < prime->range * 8; j += nb_threads)
+    {
+        if(is_prime(prime->nb + j))
+        {
+            /*fprintf(stderr, "%" PRIu64 " is prime (tid: %" PRIu64 ")\n", prime->nb + j, tid);*/
+            yotta_atomic_fetch_or(thread_prime->results + j / 8, (1u << (j % 8)));
+        }
+    }
+}
+
+static
+void
+is_prime_range_dispatch(prime_t * prime)
+{
+    uint8_t * results = yotta_alloc_sa(uint8_t, prime->range);
+    memset(results, 0, prime->range);
+
+    thread_prime_t params;
+    params.prime = prime;
+    params.results = results;
+
+    // Dispatch the computation to all available cores
+    yotta_dispatch((yotta_dispatch_func_t) thread_is_prime, &params, 0);
+
+    /*fprintf(stderr, "Dispatch DONE\n");*/
+
+    // Send the results to the master
+    yotta_sync_t sync;
+    yotta_push_package(prime->master_addr, prime->range, results, &sync);
+    yotta_sync_wait(&sync);
+
+    /*fprintf(stderr, "Push DONE\n");*/
+
+    yotta_free(results);
 }
 
 int
@@ -62,8 +138,6 @@ main(int argc, char const * const * argv)
         return 1;
     }
 
-    fprintf(stderr, "Initialized\n");
-
     if (yotta_context_connect(&context, "127.0.0.1", 5000))
     {
         return 1;
@@ -71,22 +145,31 @@ main(int argc, char const * const * argv)
 
     fprintf(stderr, "Connected\n");
 
-    uint8_t result = 2;
+    uint8_t results[RANGE_SIZE];
+    memset(results, 1, RANGE_SIZE);
 
     prime_t prime;
-    prime.nb = 43;
-    prime.master_addr = yotta_addr(&result);
 
-    /*is_prime(&prime);*/
+    prime.nb = 2;
+    prime.range = RANGE_SIZE;
+    prime.master_addr = yotta_addr(&results);
 
-    if (yotta_context_massive(&context, (yotta_massive_command_entry_t) is_prime, sizeof(prime), &prime, 0))
+    /*is_prime_range_dispatch(&prime);*/
+
+    if (yotta_context_massive(&context, (yotta_massive_command_entry_t) is_prime_range, sizeof(prime), &prime, 0))
     {
         return 1;
     }
 
     fprintf(stderr, "Massived\n");
 
-    fprintf(stderr, "%" PRIu64 " is prime ? %d\n", prime.nb, result);
+    for (size_t i = 0; i < prime.range; i++)
+    {
+        for (size_t j = 0; j < 8; j++)
+        {
+            fprintf(stderr, "%" PRIu64 " is prime: %d\n", prime.nb + i * 8 + j, (results[i] & (1u << j)) >> j);
+        }
+    }
 
     if (yotta_context_destroy(&context))
     {
