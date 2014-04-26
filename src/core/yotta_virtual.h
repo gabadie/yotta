638d1ff2340983c78fee3b39d6adc7b34de1cde6
Guillaume Abadie <guillaume.abadie@gmail.com>
2014-04-26 21:09:28 +0200

    Bug #10: Implements yotta_{vtable_set,vcall_release}() in src/core/yotta_virtual.h

--- src/core/yotta_virtual.h
+++ src/core/yotta_virtual.h
@@ -1,6 +1,9 @@
 #ifndef _YOTTA_VIRTUAL
 #define _YOTTA_VIRTUAL
 
+#include "yotta_struct.h"
+
+
 /*
  * Gets an object's virtual table
  *
@@ -9,6 +12,15 @@
  */
 #define yotta_vtable(this,vtable_type) \
     (*(vtable_type const **) (this))
+
+/*
+ * Sets an object's virtual table
+ *
+ * @param <this> is the pointer on the object
+ * @param <vtable> is the vtable to set
+ */
+#define yotta_vtable_set(this,vtable) \
+    ((*(void const **) (this)) = (void const *)(vtable))
 
 /*
  * Gets an object's virtual function
@@ -31,8 +43,19 @@
  * @example:
  *  yotta_vcall(my_object, my_vtable_t, my_function_name, foo, bar)
  */
-#define yotta_vcall(this,vtable_type,function_name,...) \
-    (yotta_vfunc(this, vtable_type)(this, __VA_ARGS__))
+#define yotta_vcall(this,vtable_type,function_name) \
+    (yotta_vfunc(this, vtable_type, function_name)(this))
+
+#define yotta_vcall_params(this,vtable_type,function_name,...) \
+    (yotta_vfunc(this, vtable_type, function_name)(this, __VA_ARGS__))
+
+/*
+ * Calls an object's virtual release function
+ *
+ * @param <this> is the pointer on the object to release
+ */
+#define yotta_vcall_release(this) \
+    ((**((void (***)(typeof(this))) (this)))(this))
 
 
 #endif
